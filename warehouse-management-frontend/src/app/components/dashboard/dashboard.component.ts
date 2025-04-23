0c884f6ca40ae7b74974521c518913a37702aef6
Jonathan-M97 <jonathan@teamapollo.com>
2025-04-23 14:32:24 -0400

    Dashboard's Alerts section is updated to show alerts from database. -JM

--- warehouse-management-frontend/src/app/components/dashboard/dashboard.component.ts
+++ warehouse-management-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../../services/alert.service';
+import { Alert } from '../../models/Alert';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  alert: Alert[] = [];
+
+    constructor(private alertService: AlertService) {}
+
+    ngOnInit(): void {
+      this.loadAlerts();
+    }
+
+    loadAlerts(): void {
+      this.alertService.getAllAlerts().subscribe(data => {
+        this.alert = data;
+      })
+    }
 
 }
