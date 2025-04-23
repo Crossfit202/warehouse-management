ea6844220544251c351467422531f2b5e7dbb720
Jonathan-M97 <jonathan@teamapollo.com>
2025-04-23 17:22:06 -0400

    Updated Alerts page so you can select which warehouse to see the alerts of. -JM

--- warehouse-management-frontend/src/app/components/alerts/alerts.component.ts
+++ warehouse-management-frontend/src/app/components/alerts/alerts.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Alert } from '../../models/Alert';
 import { AlertService } from '../../services/alert.service';
+import { WarehouseService } from '../../services/warehouse.service';
+import { Warehouse } from '../../models/Warehouse';
 
 @Component({
   selector: 'app-alerts',
@@ -12,12 +14,15 @@ import { AlertService } from '../../services/alert.service';
 })
 export class AlertsComponent implements OnInit {
 
-  alerts: Alert[] = [];
+  alerts: Alert[] = []; // an array for the alerts
+  warehouses: Warehouse[] = [];   // an array for the warehouses
+  selectedWarehouse: string = ''; // property to hold the warehouse a user selects
 
-  constructor(private alertService: AlertService) { }
+  constructor(private alertService: AlertService, private warehouseService: WarehouseService) { }
 
   ngOnInit(): void {
     this.loadAlerts();
+    this.loadWarehouses(); 
   }
 
   loadAlerts(): void {
@@ -26,6 +31,24 @@ export class AlertsComponent implements OnInit {
         this.alerts = data;
       }
     )
+  }
+
+  loadWarehouses(): void {
+    this.warehouseService.getAllWarehouses().subscribe(data => {
+      this.warehouses = data;
+      
+      // input the first warehouse name to 
+      if (this.warehouses.length > 0) {
+        this.selectedWarehouse = this.warehouses[0].name;
+      }
+  
+    });  
+  }
+
+  // method to handle drop-down list change
+  onSelect(event: Event): void{
+    const input = event.target as HTMLInputElement;
+    this.selectedWarehouse = input.value;
   }
 
 }
